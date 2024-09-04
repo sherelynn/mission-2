c7a3aced2c906278bd9872974777b83e90457041
sherelynn <sherelynnb@missionreadyhq.com>
2024-09-04 13:11:27 +1200

    Add computer player feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===== Variables for the elements ===== //
-const cells = document.getElementsByTagName("td") // Array of all the cells in the table
+const cells = Array.from(document.getElementsByTagName("td")) // Array of all the cells in the table
 const subtitle = document.getElementById("subtitle") // Dynamic subtitle
 const oCountWins = document.getElementById("o-count") // Displays "O" wins
 const xCountWins = document.getElementById("x-count") // Displays "X" wins
@@ -20,6 +20,18 @@ let clicks = 0 // Clicks counter
 let oCount = 0 // "O" wins counter
 let xCount = 0 // "X" wins counter
 
+// Array of winning combinations
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 // Function to play sound
 const playSound = soundPath => {
   const sound = new Audio(soundPath)
@@ -35,59 +47,47 @@ const switchPlayer = () => {
   subtitle.textContent = noughtsTurn
     ? `It's ⭕️ Nought's Turn`
     : `It's ❌ Cross's Turn`
+
+  console.log("noughtsTurn", noughtsTurn)
+  return noughtsTurn // To be used for computer's move
 }
 
-// Function to handle the click cell event
-const handleClickCell = event => {
-  // Variable to store the cell that was clicked
-  const cell = event.target
+// Function to get available cells
+const getAvailableCells = () => cells.filter(cell => cell.textContent === "")
 
-  // Play sound when cell is clicked
-  playSound(clickSoundPath)
+// Function to make a move
+const makeMove = (index, symbol) => {
+  cells[index].textContent = symbol
+  cells[index].style.color = symbol === "O" ? "darkOrange" : "khaki"
+}
 
-  // Condition: Empty cell
-  // Mark cell "X" or "O"
-  // Increment "clicks"
-  if (cell.textContent === "") {
-    // Variable for "X" or "O"
-    let symbol = noughtsTurn ? "O" : "X"
-    cell.textContent = symbol
-    cell.style.color = symbol === "O" ? "darkOrange" : "khaki"
-    clicks++
-
-    // Call the function to check for winning condition
-    checkWinningCondition(symbol)
-
-    // Condition: Game is not over && winning condition not met
-    if (!gameIsOver) {
-      // Switch to other player
-      switchPlayer()
-
-      // Condition: All cells are filled && winning condition not met
-      if (clicks === 9) {
-        subtitle.textContent = "❗️ S T A L E M A T E ❗️"
-
-        // Play sound if it's a stalemate
-        playSound(staleMateSoundPath)
-      }
+// Function to check move
+const checkMove = (move, symbol, noughtsTurn) => {
+  console.log("check-move", move, symbol, noughtsTurn)
+  makeMove(move, symbol)
+  clicks++
+  console.log("clicks", clicks)
+  checkWinningCondition(symbol)
+  if (!gameIsOver) {
+    // Switch to next player
+    const userPlayer = switchPlayer()
+    console.log("userPlayer", noughtsTurn)
+    if (!userPlayer) {
+      setTimeout(computerMove, 500)
     }
+  }
+
+  if (clicks === 9) {
+    // Condition: All cells are filled && winning condition not met
+    subtitle.textContent = "❗️ S T A L E M A T E ❗️"
+
+    // Play sound if it's a stalemate
+    playSound(staleMateSoundPath)
   }
 }
 
 // Function to check for winning condition
 const checkWinningCondition = symbol => {
-  // Array of winning combinations
-  const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-
   // Loop through each winning combination
   for (let combination of winningCombinations) {
     // Variable to keep track of the count of the symbol in the combination
@@ -112,7 +112,7 @@ const checkWinningCondition = symbol => {
 
         // Display the winning message
         subtitle.textContent =
-          symbol === "X" ? `Cross 🍁❌🍁 Wins!` : `Nought 🍁⭕️🍁 Wins!`
+          symbol === "X" ? `Cross -❌- Wins!` : `Nought -⭕️- Wins!`
 
         // Increment "xCount" && "oCount" counter
         // Display number of wins
@@ -126,6 +126,95 @@ const checkWinningCondition = symbol => {
         }
       }
     }
+  }
+}
+
+// Function to find a winning move or block move for the computer
+const findBestMove = () => {
+  const computerSymbol = "X"
+  const opponentSymbol = "O"
+
+  // Iterate through each winning combination
+  for (let combination of winningCombinations) {
+    // Retrieve the values of the cells in the combination
+    console.log("combination", combination)
+
+    const combinationValues = combination.map(index => {
+      const value = cells[index].textContent
+      console.log(`Index: ${index}, Value: ${value}`) // Debugging output
+      return value
+    })
+
+    console.log(`Combination: ${combination}, Values: ${combinationValues}`)
+
+    // Find the index of the empty cell in the combination
+    const emptyIndex = combination.find(
+      index => cells[index].textContent === ""
+    )
+
+    // Condition: Only proceed if there is an empty cell
+    if (emptyIndex !== undefined) {
+      const isWinningMove =
+        combinationValues.filter(value => value === computerSymbol).length === 2
+      const isBlockingMove =
+        combinationValues.filter(value => value === opponentSymbol).length === 2
+
+      if (isWinningMove || isBlockingMove) {
+        console.log("winning/blocking move", emptyIndex)
+        return emptyIndex
+      }
+    }
+  }
+
+  return null
+}
+
+// Function for the computer's move
+const computerMove = () => {
+  noughtsTurn = false
+  // Find the best move for the computer
+  let move = findBestMove("X")
+  console.log("best move", move)
+
+  // Make the best move or a random move
+  if (move !== null) {
+    checkMove(move, "X", noughtsTurn)
+  } else {
+    // No best move found, make a random move
+
+    const availableCells = getAvailableCells()
+    if (availableCells.length > 0) {
+      move = Number(
+        availableCells[
+          Math.floor(Math.random() * availableCells.length)
+        ].getAttribute("data-index")
+      )
+      console.log("random move", move)
+      checkMove(move, "X", noughtsTurn)
+    }
+  }
+}
+
+// Function to handle the click cell event
+const handleClickCell = event => {
+  // Variable to store the cell that was clicked
+  const cell = event.target
+
+  // Play sound when cell is clicked
+  playSound(clickSoundPath)
+
+  // Condition: Empty cell
+  // Mark cell "X" or "O"
+  // Increment "clicks"
+  if (cell.textContent === "") {
+    noughtsTurn = true
+    let symbol = "O"
+    let index = Number(cell.getAttribute("data-index"))
+    console.log("user-cell-index", index)
+    cell.textContent = symbol
+
+    // Check user player's move
+    checkMove(index, symbol, noughtsTurn)
   }
 }
 
@@ -145,8 +234,8 @@ const resetGameLayout = () => {
 // Function to handle the reset game click event
 // Scores remain
 const handleResetGame = () => {
-  // Winner starts second on the next game
-  // Restarting switches to other player
+  // Noughts starts first
+  noughtsTurn = false
   switchPlayer()
 
   // Call "resetGameLayout" function
